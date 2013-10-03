8d7b11fdd75753cc6253d20e731dbd9c3adfc267
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-10-03 19:53:15 +0900

    コメントを追加

--- lib/model-selector/ModelSelectorManagerScript.cs
+++ lib/model-selector/ModelSelectorManagerScript.cs
@@ -4,16 +4,29 @@ using System.Collections.Generic;
 
 namespace CMaker
 {
+	/// <summary>
+	/// 選択中のオブジェクトを管理するためのクラス
+	/// </summary>
 	public class ObjectSelector
 	{
+		/// <summary>
+		/// 選択中のオブジェクト，未選択の場合はnull
+		/// </summary>
 		public GameObject SelectedObject { get; private set; }
 
-		public void SetSelectedObject(GameObject target)
+		/// <summary>
+		/// あるオブジェクトを選択状態にする
+		/// </summary>
+		/// <param name="target">選択状態にしたいオブジェクト</param>
+		public void SelectObject(GameObject target)
 		{
 			SelectedObject = target;
 		}
 
-		public void UnsetSelectedObject()
+		/// <summary>
+		/// 未選択状態にする
+		/// </summary>
+		public void UnSelectObject()
 		{
 			SelectedObject = null;
 		}
