1c27a7d73b52bcbc4e250e91790476d1f499bcf2
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-10-03 19:39:06 +0900

    初期化忘れ例外の追加

---
+++ lib/model-selector/ForgotInitializeException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMake
+{
+	namespace Exception
+	{
+		public class ForgotInitializeException : System.Exception
+		{
+			public ForgotInitializeException(string message) : base(message) { }
+		}
+	}
+}
